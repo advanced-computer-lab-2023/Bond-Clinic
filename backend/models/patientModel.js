083bcd55b31ff9165d97917fbfcf49ffb681235b
Youssef Mostafa <yousef.abdelzaher@gmail.com>
2023-10-09 18:41:38 +0300

    Task 18

--- backend/models/patientModel.js
+++ backend/models/patientModel.js
@@ -1,55 +1,81 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: 3,
-    }, 
-    name: {
-        type: String,
-        required: true,
-    }, 
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    }, 
-    password: {
-        type: String,
-        required: true,
-        minlength: 8,
-    }, 
-    dob: {
-        type : Date ,
-    }, 
-    gender: {
-        type: String,
-        required: true,
-    },
-    phoneNumber: {
-        type: Number,
-        required: true,
-        minlength: 11,
-    }, 
-    emergencyFullName: {
-        type: String,
-        required: true,
-    },
-    emergencyPhoneNumber:{
-        type: Number,
-        required: true,
-        minlength: 11,
-    },
-    doctor:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Doctor'
-    },
-    packages:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Package',
-    },
+const familyMemberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  nationalID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  relationToPatient: {
+    type: String,
+    enum: ["wife", "husband", "child"],
+    required: true,
+  },
 });
 
-export default mongoose.model('Patient',patientSchema)
+const patientSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  dob: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: true,
+    minlength: 11,
+  },
+  emergencyFullName: {
+    type: String,
+    required: true,
+  },
+  emergencyPhoneNumber: {
+    type: Number,
+    required: true,
+    minlength: 11,
+  },
+  doctor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Doctor",
+  },
+  packages: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Package",
+  },
+  familyMembers: [familyMemberSchema], // Array of family members
+});
+
+export default mongoose.model("Patient", patientSchema);
--- front/src/pages/patient.js
+++ front/src/pages/patient.js
@@ -1,8 +1,65 @@
 import React, { useState, useEffect} from "react";
 import axios from 'axios'
+import '../styles/doctor.css'
 export default function Patient(){
     const [users, setUsers] = useState([]);
     const [selectedUser, setSelectedUser] = useState(null);
+    const [message, setMessage] = useState("");
+    const [newFamilyMember, setNewFamilyMember] = useState({
+      username:"",
+      name: "",
+      nationalID: "",
+      age: "",
+      gender: "male",
+      relationToPatient: "child", 
+    });
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setNewFamilyMember({
+        ...newFamilyMember,
+        [name]: value,
+      });
+    };
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+  
+
+      if(newFamilyMember.age==="" || newFamilyMember.gender===""||newFamilyMember.relationToPatient===""
+         || newFamilyMember.username===""||newFamilyMember.nationalID===""){
+          return setMessage('All Fields MUST be Input')
+         }
+
+
+      // Send a PATCH request to your backend API to add the new family member
+      axios
+        .patch("http://localhost:4000/api/patient/addfamily", newFamilyMember) // Replace with the actual API endpoint
+        .then((response) => {
+          // Handle the response as needed
+          console.log("New family member added:", response.data);
+          setMessage(response.data.familyMembers.pop().name+" Has been added successfully")
+          // Clear the form input fields
+      setNewFamilyMember({
+        username:"",
+        name: "",
+        nationalID: "",
+        age: "",
+        gender: "male",
+        relationToPatient: "child", // Reset the relation field
+      });
+          // You can also update the 'users' state to reflect the changes if needed
+        })
+        .catch((error) => {
+          console.error("Error adding family member:", error);
+          setMessage("Error while adding family member")
+        });
+  
+      
+    };
+
+
+
     const handleUserClick = (user) => {
         setSelectedUser(user);
       };
@@ -17,7 +74,77 @@ export default function Patient(){
             console.error("Error fetching user data:", error);
           });
       }, []);
-    return(
+    return(      
+      <>
+
+<div>
+        {/* Add a form for adding a new family member */}
+        <form onSubmit={handleSubmit}>
+        <h2>Add a New Family Member</h2>
+          
+        <label>Your Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={newFamilyMember.username}
+            onChange={handleInputChange}
+          />
+
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={newFamilyMember.name}
+            onChange={handleInputChange}
+          />
+
+          <label>National ID:</label>
+          <input
+            type="text"
+            name="nationalID"
+            value={newFamilyMember.nationalID}
+            onChange={handleInputChange}
+          />
+
+          <label>Age:</label>
+          <input
+            type="number"
+            name="age"
+            value={newFamilyMember.age}
+            onChange={handleInputChange}
+          />
+
+          <label>Gender:</label>
+          <select
+            name="gender"
+            onChange={handleInputChange}
+            value={newFamilyMember.gender}
+          >
+            <option  value="male">Male</option>
+            <option  value="female">Female</option>
+          </select>
+
+          <label>Relation to Patient:</label>
+          <select
+            name="relationToPatient"
+            onChange={handleInputChange}
+            value={newFamilyMember.relationToPatient}
+          >
+            <option value="wife">Wife</option>
+            <option value="husband">Husband</option>
+            <option value="child">Child</option>
+          </select>
+
+          <button type="submit" className="button-78">Add Family Member</button>
+        </form>
+        {message && <div className="Message"  >{message} </div>}
+      </div>
+
+
+      
+
+
+
         <div>
       <h2 className="table-name">Doctor List</h2>
       <table className="user-table">
@@ -57,6 +184,7 @@ export default function Patient(){
         </tbody>
       </table>
     </div>
+    </>
     );
 }
     

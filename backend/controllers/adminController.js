7e6ae974f07d83855207b0ac9c95b560a3b3f7f3
Zedomancer <warriorofrock8451@yahoo.com>
2023-11-08 20:54:23 +0200

    S2 backend 10,15,16,17
    Completed backend for requirement 10,15,16,17. 4 was not completed. Frontend needs to be done.

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import adminModel from "../models/adminModel.js";
-
+import doctorModel from "../models/doctorModel.js";
 
 export const createAdmin = async (req, res) => {
   const {
@@ -26,3 +26,58 @@ export const deleteAdmin = async (req, res) => {
     res.status(400).json({error: error.message});
   }
 }
+//10
+// Get a list of pending doctor registration requests
+export const getPendingDoctorRequests = async (req, res) => {
+  try {
+    const pendingDoctors = await doctorModel.find({ status: "pending" });
+    res.status(200).json(pendingDoctors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Accept a doctor request
+export const acceptDoctorRequest = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const doctor = await doctorModel.findOneAndUpdate(
+      { username },
+      { status: "accepted" },
+      { new: true }
+    );
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// Reject a doctor request
+export const rejectDoctorRequest = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const doctor = await doctorModel.findOneAndUpdate(
+      { username },
+      { status: "rejected" },
+      { new: true }
+    );
+    res.status(200).json(doctor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+//15
+// Accept a doctor request
+export const acceptDoctorRequestReg = async (req, res) => {
+  const { username } = req.params;
+  try {
+      const doctor = await doctorModel.findOneAndUpdate(
+          { username },
+          { status: "accepted" },  // Update the status to "accepted"
+          { new: true }
+      );
+      res.status(200).json(doctor);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
